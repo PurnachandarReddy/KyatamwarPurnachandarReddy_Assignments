66709cae9628be7d17e32e96109d80bc64ffae78
PurnachandarReddy <86964959+PurnachandarReddy@users.noreply.github.com>
2021-08-02 22:54:32 +0530

    Add files via upload

---
+++ Spring Framework Assignments/SpringCore/set/QuestionSetDemo.java
@@ -0,0 +1,16 @@
+package com.set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class QuestionSetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/set/configset.xml");
+		QuestionSet quest=(QuestionSet) context.getBean("qset");
+		System.out.println(quest);
+	}
+
+}
