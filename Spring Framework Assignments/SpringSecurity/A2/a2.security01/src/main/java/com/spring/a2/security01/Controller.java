a2b21880e4e780668fc87f03a3ccf5a8a4254563
PurnachandarReddy <86964959+PurnachandarReddy@users.noreply.github.com>
2021-08-03 23:21:49 +0530

    Add files via upload

---
+++ Spring Framework Assignments/SpringSecurity/A2/a2.security01/src/main/java/com/spring/a2/security01/Controller.java
@@ -0,0 +1,24 @@
+package com.spring.a2.security01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+	@RequestMapping("/")
+	public String demo() {
+		return "Hello Everyone";
+	}
+	
+	@RequestMapping("/user")
+	public String user() {
+		return "Hello.. user";
+		
+	}
+
+	
+	@RequestMapping("/admin")
+	public String admin() {
+		return "Hello.. admin";
+		
+
+}}
