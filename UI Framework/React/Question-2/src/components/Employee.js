7b6e99a3ff5d054185947fcc5dbe724e3a055da5
PurnachandarReddy <86964959+PurnachandarReddy@users.noreply.github.com>
2021-08-21 22:47:08 +0530

    Add files via upload

---
+++ UI Framework/React/Question-2/src/components/Employee.js
@@ -0,0 +1,32 @@
+import React,{component} from 'react'
+
+function Employee() {
+    return (
+        <div>
+           <table>
+                <tr>
+                   <th>Name</th>
+                   <th>Desc</th>
+                </tr>
+                <tr>
+                    <td>KPCReddy</td>
+                    <td>CEO</td>
+                </tr>   
+                <tr>
+                    <td>Kamal</td>
+                    <td>MD</td>
+                </tr>
+                <tr>
+                    <td>Purna</td>
+                    <td>chairman</td>
+                </tr>
+                <tr>
+                    <td>Pavan</td>
+                    <td>CEO</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default Employee
